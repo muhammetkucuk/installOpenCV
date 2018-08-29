a8f600e0b82fd7b748816a405c1de6c36f89042e
Muhammet Küçük <iletisim@muhammetkucuk.com>
2018-08-29 10:20:11 +0300

    script update

--- installOpenCV.sh
+++ installOpenCV.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 sudo apt-get update -y &&
 sudo apt-get upgrade -y &&
+sudo apt-get remove x264 libx264-dev -y &&
 sudo apt-get dist-upgrade -y &&
 sudo apt-get autoremove -y &&
-sudo apt-get install build-essential cmake -y &&
+sudo apt-get install build-essential cmake cmake-qt-gui -y &&
 sudo apt-get install zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libopenexr-dev libgdal-dev -y &&
 sudo apt-get install libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev -y && sudo apt-get install libgtk2.0-0 libgtk2.0-dev libjpeg8 libjpeg8-dev yasm -y &&
+sudo apt-get install libatlas-base-dev -y &&
+sudo apt-get install libavdevice-dev libavresample-dev libass-dev libfdk-aac-dev libfreetype6-dev libopus-dev libvpx-dev libx265-dev -y  &&
+sudo apt-get install x264 x265 v4l-utils -y &&
+sudo apt-get install libprotobuf-dev protobuf-compiler -y &&
+sudo apt-get install libgoogle-glog-dev libgflags-dev -y &&
+sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen -y &&
 sudo apt-get install libtbb-dev libeigen3-dev -y &&
-sudo apt-get install python-dev python-tk python-numpy python3-dev python3-tk python3-numpy &&
+sudo apt-get install python-dev python-tk python-numpy python3-dev python3-tk python3-numpy -y &&
 sudo apt-get install libgstreamer1.0-dev -y &&
-sudo apt-get install git qt5-default -y &&
+sudo apt-get install git gfortran qt5-default -y &&
 sudo apt-get install libvtk7-dev -y &&
 echo "/usr/local/lib" >> opencv.conf &&
 sudo mv opencv.conf /etc/ld.so.conf.d/opencv.conf &&
 wget https://github.com/opencv/opencv/archive/3.4.2.tar.gz &&
-tar -xvzf 3.4.2.tar.gz && 
+tar -xvzf 3.4.2.tar.gz &&
 rm 3.4.2.tar.gz &&
 wget https://github.com/opencv/opencv_contrib/archive/3.4.2.tar.gz &&
 tar -xvzf 3.4.2.tar.gz &&
@@ -22,8 +29,8 @@ rm 3.4.2.tar.gz &&
 cd opencv-3.4.2 &&
 mkdir build &&
 cd build &&
-cmake .. -DWITH_QT:BOOL="1" -DOPENCV_EXTRA_MODULES_PATH:PATH="../../opencv_contrib-3.4.2/modules" &&
-make -j7 &&
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_QT=ON -DWITH_V4L=ON -DWITH_OPENGL=ON -DWITH_TBB=ON -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.4.2/modules &&
+make -j4 &&
 sudo make install &&
 cd ../.. &&
 sudo rm -R opencv-3.4.2/ opencv_contrib-3.4.2/ &&
